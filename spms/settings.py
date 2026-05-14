13d9b1fdd81ad170d4bcb76a1e1a268781e17e81
Daniel Kioko <danielkioko1844@gmail.com>
2026-05-14 14:02:24 +0300

    Changed Landing Page UI

--- spms/settings.py
+++ spms/settings.py
@@ -102,14 +102,23 @@ WSGI_APPLICATION = 'spms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# PostgreSQL Configuration (commented out)
+# DATABASES = {
+#     'default': {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "spms",
+#         "USER": "postgres",
+#         "PASSWORD": "1234",
+#         "HOST": "localhost",
+#         "PORT": "5432", 
+#     }
+# }
+
+# Default SQLite Database
 DATABASES = {
     'default': {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "spms",
-        "USER": "postgres",
-        "PASSWORD": "1234",
-        "HOST": "localhost",
-        "PORT": "5432", 
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
